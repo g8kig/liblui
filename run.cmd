cef1142c8cc412f2cad66479c98631a7efab93a5
Paul <g8kig@users.noreply.github.com>
2023-03-11 13:15:11 +0000

    Added windows run.cmd

---
+++ run.cmd
@@ -0,0 +1,13 @@
+setlocal
+set CURDIR=%cd%
+rd /s/q %CURDIR%\run
+mkdir %CURDIR%\run
+
+cd %CURDIR%\lua-5.4.4
+copy build\liblua54.dll %CURDIR%\run
+copy build\lua.exe %CURDIR%\run
+cd %CURDIR%\lui
+copy build\liblui.dll %CURDIR%\run
+xcopy /s/y/q samples\* %CURDIR%\run
+
+
