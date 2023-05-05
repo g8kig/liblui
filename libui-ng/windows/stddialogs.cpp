84ea4445d351f1fdaf9c9f59d4367be2617eeff5
Paul <g8kig@users.noreply.github.com>
2023-05-05 12:40:11 +0100

    Updated upstream libui_ng

--- libui-ng/windows/stddialogs.cpp
+++ libui-ng/windows/stddialogs.cpp
@@ -117,17 +117,15 @@ char *uiSaveFile(uiWindow *parent)
 static void msgbox(HWND parent, const char *title, const char *description, TASKDIALOG_COMMON_BUTTON_FLAGS buttons, PCWSTR icon)
 {
 	WCHAR *wtitle, *wdescription;
-//HRESULT hr;
+	HRESULT hr;
 
 	wtitle = toUTF16(title);
 	wdescription = toUTF16(description);
 
-//hr = TaskDialog(parent, NULL, NULL, wtitle, wdescription, buttons, icon, NULL);
-//if (hr != S_OK)
-//logHRESULT(L"error showing task dialog", hr);
+	hr = TaskDialog(parent, NULL, NULL, wtitle, wdescription, buttons, icon, NULL);
+	if (hr != S_OK)
+		logHRESULT(L"error showing task dialog", hr);
 
-  MessageBox(parent, wtitle, wdescription, MB_OK);
-  
 	uiprivFree(wdescription);
 	uiprivFree(wtitle);
 }
