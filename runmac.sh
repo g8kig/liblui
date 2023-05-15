37c1178a34f76fbe0b94b9ea0636dc071552593d
Paul <g8kig@users.noreply.github.com>
2023-05-15 19:45:34 +0100

    Added missed file.

--- runmac.sh
+++ runmac.sh
@@ -2,8 +2,8 @@ set -e
 [ -d ./run ] && rm -r ./run
 mkdir ./run
 cp lui/build/liblui.dylib ./run
-cp lua-5.4.4/build/lua ./run
-cp lua-5.4.4/build/liblua54.dylib ./run
+cp lua-5.4.6/build/lua ./run
+cp lua-5.4.6/build/liblua54.dylib ./run
 cp -r lui/samples/* ./run
 pushd ./run
 ./lua ./controls.lua
